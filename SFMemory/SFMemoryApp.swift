5edcff64172d8140da550679bd66d5998b515fb1
VIncent Caronnet <120@120.design>
2020-10-02 09:38:13 +0200

    Initial Commit

---
+++ SFMemory/SFMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFMemoryApp.swift
+//  SFMemory
+//
+//  Created by Vincent Caronnet on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SFMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
